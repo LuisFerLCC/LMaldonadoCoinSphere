d8ca4e6154f240b09e0854194804cbd18c9a12c8
LuisFerLCC <luisfern837@gmail.com>
2025-09-12 23:33:15 -0600

    Currencies table headings

--- app/src/main/java/io/luisferlcc/ulsb/coinsphere/MainActivity.kt
+++ app/src/main/java/io/luisferlcc/ulsb/coinsphere/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -14,12 +18,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import io.luisferlcc.ulsb.coinsphere.ui.theme.CoinSphereTheme
+import io.luisferlcc.ulsb.coinsphere.ui.theme.Surface
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,6 +68,36 @@ fun HomeScreen(modifier: Modifier) {
             InfoCard("Altcoin Season", "No")
         }
 
+        // Table headings
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                TableHeading(
+                    text = "#",
+                    contentAlignment = Alignment.Center
+                )
+
+                TableHeading(
+                    text = "Name",
+                    modifier = Modifier.weight(10f)
+                )
+
+                TableHeading(
+                    text = "Price",
+                    modifier = Modifier.weight(9f)
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .background(Surface)
+                    .fillMaxWidth()
+                    .height(1.dp)
+            )
+        }
+
     }
 }
 
@@ -92,6 +128,25 @@ fun InfoCard(label: String, value: String) {
                 fontSize = 18.sp
             )
         }
+    }
+}
+
+@Composable
+private fun TableHeading(
+    text: String,
+    modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.CenterStart
+) {
+    Box(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .height(40.dp),
+        contentAlignment = contentAlignment
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+        )
     }
 }
 
