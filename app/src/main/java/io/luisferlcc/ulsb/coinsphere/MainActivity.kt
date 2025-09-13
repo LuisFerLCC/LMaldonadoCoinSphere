b86757add4981be3c574ae2deeee909c6039536e
LuisFerLCC <luisfern837@gmail.com>
2025-09-12 23:38:50 -0600

    List of currencies without images

--- app/src/main/java/io/luisferlcc/ulsb/coinsphere/MainActivity.kt
+++ app/src/main/java/io/luisferlcc/ulsb/coinsphere/MainActivity.kt
@@ -1,5 +1,7 @@
 package io.luisferlcc.ulsb.coinsphere
 
+import android.icu.text.DecimalFormat
+import android.icu.text.DecimalFormatSymbols
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,6 +14,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -20,12 +26,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.luisferlcc.ulsb.coinsphere.models.Currency
+import io.luisferlcc.ulsb.coinsphere.models.currencies
 import io.luisferlcc.ulsb.coinsphere.ui.theme.CoinSphereTheme
 import io.luisferlcc.ulsb.coinsphere.ui.theme.Surface
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +56,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
-    Column (
+    val priceFormatter = DecimalFormat(
+        "$###,###.##",
+        DecimalFormatSymbols(Locale.US)
+    )
+
+    Column(
         modifier = modifier
             .fillMaxSize()
     ) {
@@ -98,6 +114,17 @@ fun HomeScreen(modifier: Modifier) {
             )
         }
 
+        // List of currencies
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            itemsIndexed(currencies) { index, currency ->
+                CurrencyCard(index + 1, currency, priceFormatter)
+            }
+        }
+
     }
 }
 
@@ -147,6 +174,58 @@ private fun TableHeading(
             text = text,
             style = MaterialTheme.typography.bodyMedium,
         )
+    }
+}
+
+@Composable
+private fun CurrencyCard(
+    listNumber: Int,
+    currency: Currency,
+    priceFormatter: DecimalFormat
+) {
+    val formattedPrice = priceFormatter.format(currency.price)
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+            .padding(top = 12.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = listNumber.toString(),
+                modifier = Modifier.padding(end = 16.dp),
+                style = MaterialTheme.typography.bodyMedium,
+            )
+
+            Row(
+                modifier = Modifier.weight(3f),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box (
+                    modifier = Modifier
+                        .padding(end = 12.dp)
+                        .clip(CircleShape)
+                        .background(Color.Red)
+                        .size(32.dp),
+                )
+
+                Text(
+                    text = currency.name,
+                )
+            }
+
+            Text(
+                text = formattedPrice,
+                modifier = Modifier.weight(2f)
+            )
+        }
     }
 }
 
