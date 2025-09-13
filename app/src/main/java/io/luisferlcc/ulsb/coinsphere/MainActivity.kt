22b1e89e16797cb5df70de21bce071f43176cba8
LuisFerLCC <luisfern837@gmail.com>
2025-09-12 23:31:04 -0600

    Top section (title and info cards)

--- app/src/main/java/io/luisferlcc/ulsb/coinsphere/MainActivity.kt
+++ app/src/main/java/io/luisferlcc/ulsb/coinsphere/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.luisferlcc.ulsb.coinsphere.ui.theme.CoinSphereTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,11 +38,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello World!",
+fun HomeScreen(modifier: Modifier) {
+    Column (
         modifier = modifier
-    )
+            .fillMaxSize()
+    ) {
+
+        // Top section
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+        ) {
+            Text(
+                text = "CoinSphere",
+                modifier = Modifier.padding(bottom = 10.dp),
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleLarge,
+            )
+
+            InfoCard("Global Market Cap", "$2.18T")
+            InfoCard("Fear & Greed", "Neutral (54)")
+            InfoCard("Altcoin Season", "No")
+        }
+
+    }
+}
+
+@Composable
+fun InfoCard(label: String, value: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 6.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(
+                    horizontal = 16.dp,
+                    vertical = 20.dp
+                )
+        ) {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            Text(
+                text = value,
+                modifier = Modifier.padding(top = 8.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+        }
+    }
 }
 
 @Preview(
