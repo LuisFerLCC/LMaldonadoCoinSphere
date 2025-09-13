fe93650f95816ff0ea785b017111f9767a34b833
LuisFerLCC <luisfern837@gmail.com>
2025-09-12 23:24:27 -0600

    Customize Material theme (dark mode only, custom colors)

--- app/src/main/java/io/luisferlcc/ulsb/coinsphere/ui/theme/Type.kt
+++ app/src/main/java/io/luisferlcc/ulsb/coinsphere/ui/theme/Type.kt
@@ -14,6 +14,14 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        color = TextDim,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
